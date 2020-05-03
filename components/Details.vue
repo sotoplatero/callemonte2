de40d6062d59133f3ffd4fb92a76e5859f1c1497
Carlos Lugones <contact@lugodev.com>
2020-05-02 21:51:24 -0400

    perf: Frontend general improvements

--- components/Details.vue
+++ components/Details.vue
@@ -1,121 +1,123 @@
 <template>
-	<b-modal centered 
-		hide-header 
-		id="modal-show" 
-		ref="modal-show"
-		:lazy="true" 
-		content-class="border-0"
-		body-class="position-static p-0" 
-		footer-class="p-2"
-		@shown="updateProduct(product)">
+  <b-modal centered
+           hide-header
+           id="modal-show"
+           ref="modal-show"
+           :lazy="true"
+           content-class="border-0"
+           body-class="position-static p-0"
+           footer-class="p-2"
+           @shown="updateProduct(product)">
 
-	  	<button type="button" class="btn back" aria-label="Close" @click.prevent="$bvModal.hide('modal-show')">
-		    <x-icon size="1.5x" class="custom-class"></x-icon>
-	  	</button>
-	  	<div class="card border-0" >
+    <button type="button" class="btn back" aria-label="Close" @click.prevent="$bvModal.hide('modal-show')">
+      <x-icon size="1.5x" class="custom-class"></x-icon>
+    </button>
+    <div class="card border-0">
 
-		    <div class="card-img-top aspect-ratio-box" v-if="product.photo">
-		      <a href="#" class="aspect-ratio-box-inside">
-		        <img :src="product.photo === true ? placeholderImage : product.photo" :alt="product.title">
-		      </a>
-		    </div>
+      <div class="card-img-top aspect-ratio-box" v-if="product.photo">
+        <a href="#" class="aspect-ratio-box-inside">
+          <img :src="product.photo === true ? placeholderImage : product.photo" :alt="product.title">
+        </a>
+      </div>
 
-		    <div class="card-body p-3">
+      <div class="card-body p-3">
 
-		        <div class="small text-secondary">
-					<span class="">{{product.location}}</span>
-					<span v-if="product.date">&bull;</span>
-					<span class="">{{product.date}}</span>
-		        </div>
+        <div class="small text-secondary">
+          <span class="">{{product.location}}</span>
+          <span v-if="product.date">&bull;</span>
+          <span class="">{{product.date}}</span>
+        </div>
 
-		        <div class="">
-					<a :href="product.url" target="_black" rel="noopener noreferrer">
-						<b>{{product.title}}</b>
-					</a>
-					<img :src="'/fav/'+product.site+'.png'" width="16" class="ml-2">
-		        </div>
+        <div class="">
+          <a :href="product.url" target="_black" rel="noopener noreferrer">
+            <b>{{product.title}}</b>
+          </a>
+          <img :src="'/fav/'+product.site+'.png'" width="16" class="ml-2">
+        </div>
 
-		        <div class="text-secondary">
-					$<b>{{product.price}}</b>
-					<a :href="'tel:' + phone" v-if="product.phones.length"  v-for="phone in product.phones">
-					{{ phone }}
-					</a>
-		        </div>
+        <div class="text-secondary">
+          $<b>{{product.price}}</b>
+          <a :href="'tel:' + phone" v-if="product.phones.length" v-for="phone in product.phones">
+            {{ phone }}
+          </a>
+        </div>
 
-		    </div>
-		</div>
+      </div>
+    </div>
 
-		<template v-slot:modal-footer>
-			<button class="btn btn-light ml-2" @click.prevent="hide">Eliminar</button>
-			<a 
-			  :href="'tel:' + product.phones[0]" 
-			  v-if="product.phones && product.phones.length" 
-			  class="btn btn-success">
-			  <b>Llamar</b>
-			</a>            
-		</template> 
+    <template v-slot:modal-footer>
+      <button class="btn btn-light ml-2" @click.prevent="hide">Eliminar</button>
+      <a
+        :href="'tel:' + product.phones[0]"
+        v-if="product.phones && product.phones.length"
+        class="btn btn-success">
+        <b>Llamar</b>
+      </a>
+    </template>
 
-		 <b-overlay 
-		    :show="$store.state.products.updating" 
-		    no-wrap 
-		    rounded 
-		    spinner-type="grow" 
-		    spinner-variant="success">
-	  	</b-overlay>
+    <b-overlay
+      :show="$store.state.products.updating"
+      no-wrap
+      rounded
+      spinner-type="grow"
+      spinner-variant="success">
+    </b-overlay>
 
-	</b-modal>
+  </b-modal>
 </template>
 
 <script>
-import { mapActions } from 'vuex'
-import { TrashIcon, XIcon }  from 'vue-feather-icons'
+  import {mapActions} from 'vuex'
+  import {TrashIcon, XIcon} from 'vue-feather-icons'
 
-export default {
-	props: [ 'selectedProduct' ],
-	components: { TrashIcon, XIcon },
-	data(){
-		return {
-			loading: false,
-			placeholderImage: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAIAAAACCAYAAABytg0kAAAAFElEQVQIW2MMDQ39z8DAwMAIYwAAKgMD/9AXrvgAAAAASUVORK5CYII='
-		}
-	},
-	computed: {
-	    product: function() {
-	      return this.selectedProduct ?
-	                this.$store.state.products.items.find( el => el.url === this.selectedProduct.url ) :
-	                this.$store.state.products.items[0]
-	    }, 
-	},
-	methods: {
-	    ...mapActions({ updateProduct: 'products/update' }),  		
-		hide(){
-			this.$bvModal.hide('modal-show')
-			this.$store.commit('products/toggleHide',this.product)
-		}
-	}
+  export default {
+    props: ['selectedProduct'],
+    components: {TrashIcon, XIcon},
+    data() {
+      return {
+        loading: false,
+        placeholderImage: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAIAAAACCAYAAABytg0kAAAAFElEQVQIW2MMDQ39z8DAwMAIYwAAKgMD/9AXrvgAAAAASUVORK5CYII='
+      }
+    },
+    computed: {
+      product: function () {
+        return this.selectedProduct ?
+          this.$store.state.products.items.find(el => el.url === this.selectedProduct.url) :
+          this.$store.state.products.items[0]
+      },
+    },
+    methods: {
+      ...mapActions({updateProduct: 'products/update'}),
+      hide() {
+        this.$bvModal.hide('modal-show')
+        this.$store.commit('products/toggleHide', this.product)
+      }
+    }
 
-};
+  }
 
 </script>
-<style>
-.aspect-ratio-box {
-  height: 0;
-  overflow: hidden;
-  padding-top: 75%;
-  background: white;
-  position: relative;
-}
-.aspect-ratio-box-inside {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-}
-.aspect-ratio-box-inside img {
-  width: 100%;
-  height: 100%;
-  object-fit: cover; /*magic*/
-}
 
+<style>
+  .aspect-ratio-box {
+    height: 0;
+    overflow: hidden;
+    padding-top: 75%;
+    background: white;
+    position: relative;
+  }
+
+  .aspect-ratio-box-inside {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
+
+  .aspect-ratio-box-inside img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover; /*magic*/
+  }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,71 +1,72 @@
 <template>
   <section class="vertical-center m-0 bg-white">
-      <div class="container ">
-        <div class="row justify-content-center text-center">
-          <div class="col-md-6">
-            <div class="d-flex justify-content-center align-items-center">
+    <div class="container ">
+      <div class="row justify-content-center text-center">
+        <div class="col-md-6">
+          <div class="d-flex justify-content-center align-items-center">
               <span>
                 <img src="/logo.png" width="40">
               </span>
-              <h2 class="ml-2">CALLEMONTE</h2>
-            </div>
-            <h4 class="mb-5 mt-2">Buscador de clasificados en Cuba</h4>
-
-            <div class="input-group mb-3">
-              <!-- <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2"> -->
-                <input 
-                  autofocus
-                  class="form-control form-control-lg" 
-                  type="text" 
-                  placeholder="¿Que quieres comprar?" 
-                  name="input"  
-                  v-model="input" 
-                  @keyup.enter="search" >
-              <div class="input-group-append">
-                <button class="btn btn-success" type="button" @click="search">
-                  <SearchIcon></SearchIcon>
-                </button>
-              </div>
-            </div>
-                 
-              <div class="form-group">
-                <a href="https://notificon.com" class="btn btn-link text-dark" target="_blank" ><b>Alertas</b></a>
-                <nuxt-link to="/about" class="btn btn-link text-secondary">Nosotros</nuxt-link>
-                <nuxt-link to="/contact" class="btn btn-link text-secondary">Contacto</nuxt-link>
-                <a href="https://medium.com/callemonte" target="_blank" class="btn btn-link text-secondary">Blog</a>
-              </div>                    
-            
+            <h2 class="ml-2">CALLEMONTE</h2>
           </div>
+          <h4 class="mb-5 mt-2">Buscador de clasificados en Cuba</h4>
+
+          <div class="input-group mb-3">
+            <!-- <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2"> -->
+            <input
+              autofocus
+              class="form-control form-control-lg"
+              type="text"
+              placeholder="¿Que quieres comprar?"
+              name="input"
+              v-model="input"
+              @keyup.enter="search">
+            <div class="input-group-append">
+              <button class="btn btn-success" type="button" @click="search">
+                <SearchIcon></SearchIcon>
+              </button>
+            </div>
+          </div>
+
+          <div class="form-group">
+            <a href="https://notificon.com" class="btn btn-link text-dark" target="_blank"><b>Alertas</b></a>
+            <nuxt-link to="/about" class="btn btn-link text-secondary">Nosotros</nuxt-link>
+            <nuxt-link to="/contact" class="btn btn-link text-secondary">Contacto</nuxt-link>
+            <a href="https://medium.com/callemonte" target="_blank" class="btn btn-link text-secondary">Blog</a>
+          </div>
+
         </div>
-            
       </div>
+
+    </div>
   </section>
 </template>
 
 <script>
-import { SearchIcon } from 'vue-feather-icons'
-export default {
-  layout: 'home',
-  components: { SearchIcon },
-  data(){
-    return {
-      input: ''
-    }
-  },  
-  methods: {
-    search() {
-      let reMaxPrice = /<\s*(\d+)/;
-      let reMinPrice = />\s*(\d+)/;
-      let input = this.input;
+  import {SearchIcon} from 'vue-feather-icons'
 
-      let pmax = reMaxPrice.test(input) ? input.match(reMaxPrice)[1] : '';
-      let pmin = reMinPrice.test(input) ? input.match(reMinPrice)[1] : '';    
-      let q = input.replace(reMinPrice,'').replace(reMaxPrice,'').trim();   
-
-      if ( this.input.length > 0 ) {
-        this.$router.push({ path: '/search', query: { q: q, pmin: pmin, pmax: pmax } });
+  export default {
+    layout: 'home',
+    components: {SearchIcon},
+    data() {
+      return {
+        input: ''
       }
-    }            
-  }  
-}
+    },
+    methods: {
+      search() {
+        let reMaxPrice = /<\s*(\d+)/;
+        let reMinPrice = />\s*(\d+)/;
+        let input = this.input;
+
+        let pmax = reMaxPrice.test(input) ? input.match(reMaxPrice)[1] : '';
+        let pmin = reMinPrice.test(input) ? input.match(reMinPrice)[1] : '';
+        let q = input.replace(reMinPrice, '').replace(reMaxPrice, '').trim();
+
+        if (this.input.length > 0) {
+          this.$router.push({path: '/search', query: {q: q, pmin: pmin, pmax: pmax}});
+        }
+      }
+    }
+  }
 </script>
