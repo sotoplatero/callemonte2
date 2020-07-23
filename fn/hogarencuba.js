46275835e21e67f1bd48e5d36b88fea36d0034f1
Damian Soto Platero <jefe.redes@gaviota.cu>
2020-07-23 09:01:26 -0400

    fulltext en higar en cuba

--- fn/hogarencuba.js
+++ fn/hogarencuba.js
@@ -1,32 +1,47 @@
 const fetch = require("node-fetch");
-var cleaner = require('./libs/cleaner');
-const moment = require('moment')
+const moment = require('moment');
+var FlexSearch = require("flexsearch");
+const { reLocations } = require('./libs/vars.js') ;
 
 exports.handler =  async (event, context, callback) => {
-    var { q, p = 1, pmin = 1, pmax = '', province = '' } = event.queryStringParameters;
+    var { q, p = 1, pmin = 1, pmax = 1000000, province = '' } = event.queryStringParameters;
     let re = /casa|apartamento|apto/i
     let data = []
+    var index = new FlexSearch();
+
+    pmin = parseInt(pmin)>0 ? parseInt(pmin) : 1;
+    pmax = parseInt(pmax)>0 ? parseInt(pmax) : 1000000;
 
     if ( re.test(q) ) {
 
         let type = q.match(re)[0];
         const response = await fetch(`https://www.hogarencuba.com/api.json`);
         const json = await response.json();
-        data = json.filter( el => {
-            return  (el.price >= pmin) && 
-                    (el.price <= pmax) && 
-                    (el.type === type)
-        }).map( el => {
-            return {
-                title: el.title,
-                price: el.price,
-                photo: el.firstImageUrl,
-                description: el.shortDescription,
-                location: el.location,
-                url: el.url,
-                date: moment(el.created_at)
-            }
-        })
+        data = json
+            .filter( el => 
+                (el.price >= pmin) && 
+                (el.price <= pmax) && 
+                el.location.match(reLocations) 
+            )
+            .forEach( el => index.add(el.id, el.description) )
+
+        let result = index.search(q, {
+            limit: 100,
+        });
+
+        data =  data
+            .filter( el => result.includes(el.id))
+            .map( el => ({
+                    title: el.title,
+                    price: el.price,
+                    photo: el.firstImageUrl,
+                    description: el.shortDescription,
+                    location: el.location,
+                    url: el.url,
+                    date: moment(el.created_at)
+                })
+            )
+        console.log(data)
     }
 
 
