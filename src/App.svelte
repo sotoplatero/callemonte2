a822df6058ab3b39e619325391028d1eb7e52f09
Damian Soto <soto.platero@gmail.com>
2020-09-18 05:26:04 -0400

    add stemmer_es

--- src/App.svelte
+++ src/App.svelte
@@ -4,9 +4,10 @@
 	import Tailwindcss from "./Tailwindcss.svelte";
 	import Product from "./components/Product.svelte";
 	import Filter from "./components/Filter.svelte";
+
 	import queryString from 'query-string';	
 	import FlexSearch from './functions/libs/Flexsearch';
-
+	import stemmer_es from 'stemmer_es';
 
 	let filters = {
 		q: '',
@@ -19,19 +20,6 @@
 	let products = [];
 
 	let searching = 0;
-
-	let indexProducts = new FlexSearch({
-		profile: "score",
-	    stemmer: "es", 
-	    filter: "es" ,	
-	    doc: {
-	        id: "url",
-	        field: [ "title", "description" ]
-	    }
-	});
-
-	let filteredProducts = [];
-	// $: filteredProducts = indexProducts.search(filters.q);
 
 	onMount(() => {
 		filters = { 
@@ -46,8 +34,6 @@
 
 	function handleSearch() {
 		history.pushState(filters, '', '?' + queryString.stringify(filters)) ;
-		// indexProducts = indexProducts.clear();
-		products = [];
 	    search();
 	}
 	
@@ -62,6 +48,16 @@
 		
 	    let { q, pmin, pmax, page, province } = filters;
 		
+		let indexProducts = new FlexSearch({
+			profile: "score",
+			stemmer: (value) => stemmer_es.stem(value),
+		 	filter: 'a al algo algunas algunos ante antes como con contra cual cuando de del desde donde durante e el ella ellas ellos en entre era erais eran eras eres es esa esas ese eso esos esta estaba estabais estaban estabas estad estada estadas estado estados estamos estando estar estaremos estará estarán estarás estaré estaréis estaría estaríais estaríamos estarían estarías estas este estemos esto estos estoy estuve estuviera estuvierais estuvieran estuvieras estuvieron estuviese estuvieseis estuviesen estuvieses estuvimos estuviste estuvisteis estuviéramos estuviésemos estuvo está estábamos estáis están estás esté estéis estén estés fue fuera fuerais fueran fueras fueron fuese fueseis fuesen fueses fui fuimos fuiste fuisteis fuéramos fuésemos ha habida habidas habido habidos habiendo habremos habrá habrán habrás habré habréis habría habríais habríamos habrían habrías habéis había habíais habíamos habían habías han has hasta hay haya hayamos hayan hayas hayáis he hemos hube hubiera hubierais hubieran hubieras hubieron hubiese hubieseis hubiesen hubieses hubimos hubiste hubisteis hubiéramos hubiésemos hubo la las le les lo los me mi mis mucho muchos muy más mí mía mías mío míos nada ni no nos nosotras nosotros nuestra nuestras nuestro nuestros o os otra otras otro otros para pero poco por porque que quien quienes qué se sea seamos sean seas seremos será serán serás seré seréis sería seríais seríamos serían serías seáis sido siendo sin sobre sois somos son soy su sus suya suyas suyo suyos sí también tanto te tendremos tendrá tendrán tendrás tendré tendréis tendría tendríais tendríamos tendrían tendrías tened tenemos tenga tengamos tengan tengas tengo tengáis tenida tenidas tenido tenidos teniendo tenéis tenía teníais teníamos tenían tenías ti tiene tienen tienes todo todos tu tus tuve tuviera tuvierais tuvieran tuvieras tuvieron tuviese tuvieseis tuviesen tuvieses tuvimos tuviste tuvisteis tuviéramos tuviésemos tuvo tuya tuyas tuyo tuyos tú un una uno unos vosotras vosotros vuestra vuestras vuestro vuestros y ya yo él éramos'.split(' '),	
+		    doc: {
+		        id: "url",
+		        field: [ "title", "description" ]
+		    }
+		});
+
 	    sites.forEach( async site => {
 			let url = `/api/${site}?q=${q}&pmin=${pmin}&pmax=${pmax}&p=${page}&province=${province}`
 			const response = await fetch(url)
@@ -70,9 +66,7 @@
 			if (response.ok) {
 				const productsSite = await response.json();
 				indexProducts.add(productsSite);
-				// indexProducts = indexProducts;
-				filteredProducts = indexProducts.search(filters.q) 
-				products = products.concat(productsSite)
+				products = indexProducts.search(filters.q) 
 			}
 	    })
 
@@ -139,8 +133,8 @@
     </div>
 
 	<div class="divide-y divide-gray-100 dark:divide-gray-900 shadow rounded-lg overflow-hidden mb-10">
-		{#if filteredProducts}
-			{#each filteredProducts as product }
+		{#if products}
+			{#each products as product }
 				<Product 
 					product={product} 
 				></Product>
