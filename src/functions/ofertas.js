67b448027e5578721eed1fe164d6b1462598fbce
Damian Soto Platero <jefe.redes@gaviota.cu>
2020-09-21 07:38:06 -0400

    udapte

--- src/functions/ofertas.js
+++ src/functions/ofertas.js
@@ -1,26 +1,33 @@
-// import fetch from "node-fetch";
-var cheerio = require('cheerio');
 var cleaner = require('./libs/cleaner');
-const moment = require('moment')
 var Browser = require("zombie");
 
 const rePhone = /((5|7)\d{7})|((24|32|33|45)\d{6})/g;
 
 exports.handler =  async (event, context, callback) => {
     var { q, p = 1, pmin = 1, pmax = '', province } = event.queryStringParameters;
-    let data = []
-    // const response = await fetch(`http://ofertas.cu/buscar?q=${q}`, {method: 'POST'} );
-    // const body = await response.text();
+    let data = [];
+
+
     browser = new Browser()
     try {
         browser.visit(`http://ofertas.cu/`, function() {
             browser.fill( 'input[name="q"]', q )
             browser.pressButton('button[type="submit"]', () => { 
-                browser.assert.element('.search-page a');
+                // console.log(browser.body)
+                let ads = browser.queryAll('.listing')
+                data = ads.map( el => {
 
-                let ads = browser.queryAll('.search-page a')
-                ads.forEach( el => {
-                    console.log(el.text)
+                    let title = browser.text('a',el);
+                    let description = browser.text('p',el);
+                    let price = ( title + description ).match(/\d+(\.\d{2})?\s?cuc/i);
+                    let phones = ( title + description ).replace().match(/\d+(\.\d{2})?\s?cuc/i);
+
+                    console.log ( {
+                        title: cleaner(title),
+                        description: description,
+                        price: price ? parseInt(price[0]) : '?',
+                    } )
+
                 })
             });
 
