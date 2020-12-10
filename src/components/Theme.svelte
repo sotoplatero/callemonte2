f8452889ef4c0c34541d332fe6529fced5739c0f
Damian Soto <soto.platero@gmail.com>
2020-12-09 21:51:15 -0500

    pulify

--- src/components/Theme.svelte
+++ src/components/Theme.svelte
@@ -1,26 +1,25 @@
 <button on:click="{change}">
-	{#if localStorage.theme  === 'light'}
+	{#if theme  === 'light'}
 
 		<svg class="h-6 w-6 inline moon"  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
 		  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
 		</svg>
-
 	{:else}
 
 		<svg class="h-6 w-6 inline sun" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
 		  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" />
 		</svg>
-
 	{/if}
 </button>
 
 <script>
-	// import { onMount } from 'svelte';
-	// let theme 
+	
+	import { onMount } from 'svelte'; 
+	let theme 
 
-	// onMount( () => {
-	// 	theme = localStorage.theme;
-	// });
+	onMount( () => {
+		theme = localStorage.theme;
+	});
 
 	function change() {
 		if ( localStorage.theme ==='light' ) {
@@ -28,6 +27,6 @@
 		} else {
 			document.querySelector('html').classList.remove('dark')
 		}
-		localStorage.theme = localStorage.theme ==='light' ? 'dark' : 'light'
+		localStorage.theme =  theme = (localStorage.theme ==='light') ? 'dark' : 'light'
 	}	
 </script>
